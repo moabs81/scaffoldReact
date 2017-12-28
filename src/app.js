516ee07582d24852afc19a00dae531f6ae30a48f
Nick Hendricks <moabs81@hotmail.com>
2017-12-28 11:21:20 -0500

    started down another module then decided to do a new microproject

--- src/app.js
+++ src/app.js
@@ -1,29 +1,20 @@
 'use strict';
 
-import { buildTableComponent } from './components/workBenchContainer/jsModules/workBenchLayout'; //import the wrapper page. parallax baby #ForNoGoodReason
-import { apiCall } from './components/application/jsModules/ajax';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DemoApp from './components/application/DemoApp';
+import { buildTableComponent } from './components/workBenchContainer/jsModules/workBenchLayout'; //import the wrapper page. parallax baby #ForNoGoodReason
+import { apiCall } from './components/application/jsModules/ajax';
 import WebPart from './components/webPart/jsModules/WebPart';
+import WebPart2 from './components/webPart2/jsModules/webPart2';
 
-const PRODUCTS = [
-    { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-    { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-    { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-    { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-    { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-    { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
-];
 
 const buildAjaxCall = function (cbReturn) {
     const buildAPIReq = {
-        baseURI: 'https://my-json-server.typicode.com/moabs81/fakeJSONServer',
-        //baseURI: 'http://localhost:3000',
+        //baseURI: 'https://my-json-server.typicode.com/moabs81/fakeJSONServer',
+        baseURI: 'http://localhost:3000',
         searchURI: '/offices',
         method: 'GET',
-        success: function (result) {            
+        success: function (result) {
             cbReturn(result);
         }
     };
@@ -32,18 +23,20 @@ const buildAjaxCall = function (cbReturn) {
 
 buildTableComponent(function (result) { //callback function returns the DOM target for your app           
     const targetDiv = result;
-    buildAjaxCall(function (result) {                        
+    buildAjaxCall(function (result) {
         ReactDOM.render(
-            <WebPart data={JSON.parse(result.currentTarget.responseText)}/>,
+            //<WebPart2 data={JSON.parse(result.currentTarget.responseText)} />,
+            <WebPart data={JSON.parse(result.currentTarget.responseText)} />,
             document.getElementById(targetDiv)
         );
     });
-    setInterval(function(){
-        buildAjaxCall(function (result) {                        
+    setInterval(function () {
+        buildAjaxCall(function (result) {
             ReactDOM.render(
-                <WebPart data={JSON.parse(result.currentTarget.responseText)}/>,
+                //<WebPart2 data={JSON.parse(result.currentTarget.responseText)} />,
+                <WebPart data={JSON.parse(result.currentTarget.responseText)} />,
                 document.getElementById(targetDiv)
             );
         });
-    }, 120000);
+    }, 10000);
 });
---
+++ src/components/webPart2/jsModules/webPart2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WebPart2 extends Component {
+    render() {
+        return (
+            <div>
+                hi!
+            </div>
+        );
+    }
+};
+
+export default WebPart2;
